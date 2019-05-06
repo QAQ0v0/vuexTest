1791f5a48bfd0eea70d5c0d075858ae94b67badc
su <sc1996@yahoo.com>
2019-05-06 20:29:24 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <p>click: {{count}} times, count is {{evenOrOdd}}</p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementIfOdd">increment if odd</button>
+    <button @click="incrementAsync">increment async</button>
+  </div>
+</template>
+
+<script>
+import {mapState,mapActions,mapGetters} from 'vuex'
+export default {
+  name: 'App',
+  computed: {
+    // evenOrOdd () {
+    //   return this.$store.getters.evenOrOdd;
+    // }
+    ...mapState(['count']),
+    ...mapGetters(['evenOrOdd'])
+  },
+  methods : {
+    // increment () {
+    //   this.$store.dispatch('increment')
+    // },
+    // decrement () {
+    //   this.$store.dispatch('decrement')
+    // },
+    // incrementIfOdd () {
+    //   this.$store.dispatch('incrementIfOdd')
+    // },
+    // incrementAsync () {
+    //   this.$store.dispatch('incrementAsync')
+    // }
+    ...mapActions(['increment','decrement','incrementIfOdd','incrementAsync'])
+  }
+}
+</script>
+
+<style>
+
+</style>
